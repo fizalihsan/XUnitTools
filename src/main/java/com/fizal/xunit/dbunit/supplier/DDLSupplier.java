fa369ae9ce1ff76edfbeb4c5dc028c6303e6771f
Fizal <fizalihsan@gmail.com>
2016-09-28 07:29:56 -0400

    Adding Travis CI file for CI build

--- src/main/java/com/fizal/xunit/dbunit/supplier/DDLSupplier.java
+++ src/main/java/com/fizal/xunit/dbunit/supplier/DDLSupplier.java
@@ -4,5 +4,5 @@ package com.fizal.xunit.dbunit.supplier;
  * Created by fmohamed on 9/13/2016.
  */
 public interface DDLSupplier {
-    String getDDLStatement(String dbObjectName);
+    String getDDL(String dbObjectName);
 }
